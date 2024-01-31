78c8d1871f7f9e0391708d442c3e1c39f53e8ecf
raj <110736511+PRITHVIRAJ25@users.noreply.github.com>
2024-01-31 13:07:20 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+  </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <b> Project by Prajwal Gowda CM,Prithviraj B,Priyanka MC</b>
+  
+</div>
+<!-- ./wrapper -->
+
+
+</body>
+</html>
